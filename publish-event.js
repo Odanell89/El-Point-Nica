ef63b1c237f0b7cc94d340d3c5f7361d40065c32
ervinluna30 <ervinluna30@gmail.com>
2025-07-14 20:33:00 +0000

    build: Deploy latest version to production

--- publish-event.js
+++ publish-event.js
@@ -18,12 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
             const locationName = document.getElementById('event-location-name').value;
             const address = document.getElementById('event-address').value;
             const price = document.getElementById('event-price').value;
-            // Get the URL from the new input field
             const imageUrl = document.getElementById('event-image').value;
 
             try {
                 await addDoc(collection(db, "events"), {
-                    creatorId: creatorId,
+                    creatorId,
                     title,
                     description,
                     category,
@@ -31,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     locationName,
                     address,
                     price: price ? Number(price) : 0,
-                    imageUrl: imageUrl, // Save the provided URL
+                    imageUrl,
                     approved: false
                 });
                 alert("Event submitted successfully for review!");
